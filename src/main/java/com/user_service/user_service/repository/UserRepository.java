0967a7151c099643d9d475835a57d521e592c325
antyagi-10 <antyagi@grepruby.io>
2025-12-16 17:50:25 +0530

    create custom Query : updateUser

--- src/main/java/com/user_service/user_service/repository/UserRepository.java
+++ src/main/java/com/user_service/user_service/repository/UserRepository.java
@@ -2,9 +2,32 @@ package com.user_service.user_service.repository;
 
 import com.user_service.user_service.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(
+            "UPDATE UserEntity u " +
+                    "SET u.username = COALESCE(:username, u.username),"+
+                    "u.email = COALESCE(:email, u.email), " +
+                    "u.role = COALESCE(:role, u.role), " +
+                    "u.password_hash = COALESCE(:password , u.password_hash)," +
+                    "u.updated_at = CURRENT_TIMESTAMP " +
+                    "WHERE u.Id = :id"
+    )
+    int updateUser(
+            @Param("id") Integer id,
+            @Param("username") String username,
+            @Param("email") String email,
+            @Param("role") UserEntity.Role role,
+            @Param("password") String password
+    );
 }
