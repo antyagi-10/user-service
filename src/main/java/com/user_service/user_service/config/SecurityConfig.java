ff9d707b4e5bcb4ae26624d1757c0e566780e3f5
antyagi-10 <antyagi@grepruby.io>
2025-12-17 18:40:24 +0530

    Generate Jwt Token at user login

---
+++ src/main/java/com/user_service/user_service/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.user_service.user_service.config;
+
+import com.user_service.user_service.entity.UserEntity;
+import com.user_service.user_service.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v1/users/login",
+                                "/v1/users/register"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+
+                );
+        return http.build();
+    }
+
+
+        @Bean
+    public AuthenticationManager authenticationManager() {
+        return authentication -> {
+            String username = authentication.getName();
+            String password = authentication.getCredentials().toString();
+
+            UserEntity user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+            if (!passwordEncoder().matches(password, user.getPassword_hash())) {
+                throw new BadCredentialsException("Invalid password");
+            }
+
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+            );
+        };
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/user_service/user_service/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.user_service.user_service.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+@Value("${security.jwt.secret-key}")
+private String SECRET;
+@Value("${security.jwt.expiration-time}")
+private long EXPIRATION;
+
+    public long getExpirationTime() {
+        return EXPIRATION;
+    }
+
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken( Map<String, Object> claims,  String username){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration( new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
