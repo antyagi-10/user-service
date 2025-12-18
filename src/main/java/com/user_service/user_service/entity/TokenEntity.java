c057542f5571c33c6b9f5d2a4c4b0e8b585bb824
antyagi-10 <antyagi@grepruby.io>
2025-12-18 16:37:52 +0530

    create Token Entity and Token Repository

---
+++ src/main/java/com/user_service/user_service/entity/TokenEntity.java
@@ -0,0 +1,48 @@
+package com.user_service.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tokens")
+public class TokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id" , nullable = false)
+    private Integer id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tokenType")
+    private TokenType tokenType;
+
+    @Column(name = "expired")
+    private Boolean expired;
+
+    @Column(name = "revoked")
+    private Boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    public enum TokenType{
+       Bearer
+    }
+
+}
