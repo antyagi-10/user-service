350ab85f9bef36dc100eba09cd3260835a92ab2e
antyagi-10 <antyagi@grepruby.io>
2025-12-22 14:06:35 +0530

    resolved conflicts

--- src/main/java/com/user_service/user_service/service/UserServiceImpl.java
+++ src/main/java/com/user_service/user_service/service/UserServiceImpl.java
@@ -43,5 +43,13 @@ public class UserServiceImpl implements UserService{
         );
         return userRepository.findById(id).get();
     }
+
+    public boolean deleteUser(Integer id) {
+        if(userRepository.findById(id).isEmpty()){
+            throw new UserNotFoundException("User not Found");
+        }
+        userRepository.deleteById(id);
+        return true;
+    }
 }
 
--- src/main/java/com/user_service/user_service/v1/controller/UserControllerImpl.java
+++ src/main/java/com/user_service/user_service/v1/controller/UserControllerImpl.java
@@ -30,4 +30,10 @@ public class UserControllerImpl implements UserController{
         return ResponseEntity.ok(UserMapper.toDto(updated));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        boolean deleted = userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
 }
