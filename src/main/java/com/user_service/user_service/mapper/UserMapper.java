e1b643a6fc4f8c8f0e66a597be45cfb4b91b0f43
antyagi-10 <antyagi@grepruby.io>
2025-12-22 12:41:17 +0530

    Resolved PR Suggestions: fix indentation

--- src/main/java/com/user_service/user_service/mapper/UserMapper.java
+++ src/main/java/com/user_service/user_service/mapper/UserMapper.java
@@ -4,14 +4,14 @@ import com.user_service.user_service.dto.RegisterResponseDTO;
 import com.user_service.user_service.entity.UserEntity;
 
 public class UserMapper {
-        public static RegisterResponseDTO toDto(UserEntity user) {
-            return new RegisterResponseDTO(
-                    user.getId(),
-                    user.getUsername(),
-                    user.getEmail(),
-                    user.getCreated_at(),
-                    user.getUpdated_at()
-            );
-        }
+    public static RegisterResponseDTO toDto(UserEntity user) {
+        return new RegisterResponseDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getCreated_at(),
+                user.getUpdated_at()
+        );
     }
+}
 
